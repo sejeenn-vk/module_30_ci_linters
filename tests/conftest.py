5ef42a071ce9148e55d02e3fc93a29259130c6a5
Eugene Vorontsov <sejeenn@gmail.com>
2024-10-23 22:22:32 +0300

    все переделано под 29 hw

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,5 @@
-import asyncio
-from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from fastapi import FastAPI
 import pytest
-
-engine = create_async_engine("sqlite+aiosqlite://", echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
